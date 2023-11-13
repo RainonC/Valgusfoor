834357f464c55aa1093c32b248aae7f4b52682ea
RainonC <118743359+RainonC@users.noreply.github.com>
2023-11-13 13:38:25 +0200

    Add project files.

---
+++ Valgusfoor/Valgusfoor/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Valgusfoor
+{
+    public partial class MainPage : ContentPage
+    {
+        List<ContentPage> pages = new List<ContentPage>() {  new Valgusfoor_Page() };
+        List<string> teksts = new List<string> {  "Ava töö Valgusfoor" };
+        StackLayout st;
+        public MainPage()
+        {
+            st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.YellowGreen
+            };
+            for (int i = 0; i < pages.Count; i++)
+            {
+                Button button = new Button
+                {
+                    Text = teksts[i],
+                    TabIndex = i,
+                    BackgroundColor = Color.Red,
+                    TextColor = Color.White
+                };
+                st.Children.Add(button);
+                button.Clicked += Button_Clicked;
+            }
+            ScrollView sv = new ScrollView { Content = st };
+            Content = sv;
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            await Navigation.PushAsync(pages[btn.TabIndex]);
+        }
+    }
+}
---
+++ Valgusfoor/Valgusfoor/Valgusfoor_Page.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARgv22_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Valgusfoor_Page : ContentPage
+    {
+        Frame fr;
+        List<Color> colors = new List<Color> { Color.Red, Color.Yellow, Color.Green };
+        List<string> reaktion = new List<string>() { "Seisa", "Oota", "Mine" };
+        List<Frame> frames = new List<Frame>();
+        Button btn;
+        List<string> tekst = new List<string>() { "Sisse", "Välja" };
+        StackLayout st = new StackLayout();
+        StackLayout vf = new StackLayout();
+        StackLayout nd = new StackLayout();
+        public Valgusfoor_Page()
+        {
+            vf.Margin = new Thickness(40);
+            for (int i = 0; i < 3; i++)
+            {
+                fr = new Frame
+                {
+                    Content = new Label { Text = "Lülita sisse" },
+                    HasShadow = true,
+                    TabIndex = i,
+                    BackgroundColor = Color.Gray,
+                    CornerRadius = 70,
+                    HeightRequest = 150,
+                    BorderColor = Color.Black
+                };
+                frames.Add(fr);
+                vf.Children.Add(fr);
+                TapGestureRecognizer tap = new TapGestureRecognizer();
+                tap.Tapped += Tap_Tapped;
+                fr.GestureRecognizers.Add(tap);
+            }
+            nd.Margin = new Thickness(10);
+            nd.HorizontalOptions = LayoutOptions.CenterAndExpand;
+            nd.Orientation = StackOrientation.Horizontal;
+            for (int i = 0; i < 2; i++)
+            {
+                btn = new Button
+                {
+                    Text = tekst[i],
+                    BackgroundColor = Color.AntiqueWhite,
+                    TabIndex = i,
+                    HorizontalOptions = LayoutOptions.Fill
+                };
+                nd.Children.Add(btn);
+                btn.Clicked += Btn_Clicked;
+            }
+            st.Children.Add(vf);
+            st.Children.Add(nd);
+            Content = st;
+        }
+        bool startstop = false;
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            if (startstop)
+            {
+                Frame frame = sender as Frame;
+                frame.Content = new Label { Text = reaktion[frame.TabIndex] };
+            }
+        }
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text == "Sisse")
+            {
+                startstop = true;
+                for (int i = 0; i < 3; i++)
+                {
+                    frames[i].BackgroundColor = colors[i];
+                    frames[i].Content = new Label { Text = "Tee oma valik" };
+                }
+            }
+            else
+            {
+                startstop = false;
+                for (int i = 0; i < 3; i++)
+                {
+                    frames[i].BackgroundColor = Color.Gray;
+                    frames[i].Content = new Label { Text = "Lülita sisse" };
+                }
+            }
+
+        }
+    }
+}
